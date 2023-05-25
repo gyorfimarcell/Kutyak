d18b2271bcdb3b06565c8c5b07327bfc6399c2da
Györfi Marcell <gyorfimarcell@gmail.com>
2023-05-25 11:40:29 +0200

    Osztály

---
+++ Kutya.cs
@@ -0,0 +1,29 @@
+public class Kutya
+{
+    string id;
+    string fajta;
+    string eredetiFajta;
+    string nev;
+    int eletkor;
+    DateTime utolsoEllenorzes;
+
+    public Kutya(string csvSor, List<string[]> nevSorok, List<string[]> fajtaSorok)
+    {
+        string[] mezok = csvSor.Split(';');
+        string[] fajtaSor = fajtaSorok.Find(x => x[0] == mezok[1]);
+
+        this.id = mezok[0];
+        this.fajta = fajtaSor[1];
+        this.eredetiFajta = fajtaSor[2];
+        this.nev = nevSorok.Find(x => x[0] == mezok[2])[1];
+        this.eletkor = Convert.ToInt32(mezok[3]);
+        this.utolsoEllenorzes = DateTime.Parse(mezok[4]);
+    }
+
+    public string Id { get => id; }
+    public string Fajta { get => fajta; }
+    public string EredetiFajta { get => eredetiFajta; }
+    public string Nev { get => nev; }
+    public int Eletkor { get => eletkor; }
+    public DateTime UtolsoEllenorzes { get => utolsoEllenorzes; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string[]> nevek = File.ReadAllLines("KutyaNevek.csv").Skip(1).Select(x => x.Split(";")).ToList();
+        List<string[]> fajtak = File.ReadAllLines("KutyaFajtak.csv").Skip(1).Select(x => x.Split(";")).ToList();
+
+        List<Kutya> kutyak = File.ReadAllLines("Kutyak.csv").Skip(1).Select(x => new Kutya(x, nevek, fajtak)).ToList();
+
+        Console.WriteLine($"3. feladat: Kutyanevek száma: {nevek.Count}");
+    }
+}
